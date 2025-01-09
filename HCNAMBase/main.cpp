fe7a886f7605afc7c2b6e9c0682379bbcc279a34
Simo Erkinheimo <simo.erkinheimo@gmail.com>
2025-01-09 23:56:30 +0200

    Preliminary main class and cmake conf

---
+++ HCNAMBase/main.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "generated.hpp"
+
+namespace HCNAMBase
+{
+
+    static HCNAMBase::NAMModel zmoModel;
+
+    void Load() // Temporary
+    {
+        //zmoModel.load_weights
+    }
+
+    void Reset(int sample_count)
+    {
+        zmoModel.prepare(sample_count);
+        zmoModel.prewarm();
+    }
+
+    void Process(float* in, float* out, int sample_count)
+    {
+        zmoModel.forward(in, out, sample_count);
+    }
+
+} // HCNAMBase
